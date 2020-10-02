6b8ea95de4c1b58537431790a0440dacf70e903f
TheWrigglyGhost <serenokaisser@gmail.com>
2020-10-02 10:26:33 +0800

    Ladder tiles and ability to climb ladder

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,6 +8,7 @@ public class Player : MonoBehaviour
     // Config
     [SerializeField] float runSpeed = 7f;
     [SerializeField] float jumpSpeed = 5f;
+    [SerializeField] float climbSpeed = 4f;
 
     // State
     bool isAlive = true;
@@ -31,6 +32,7 @@ public class Player : MonoBehaviour
         Run();
         FlipSprite();
         Jump();
+        ClimbLadder();
     }
 
     private void Run()
@@ -62,5 +64,17 @@ public class Player : MonoBehaviour
         {
             transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x), 1f);
         }
+    }
+
+    private void ClimbLadder()
+    {
+        if (!myCollider2D.IsTouchingLayers(LayerMask.GetMask("Ladder"))) { return; }
+
+        float ladderClimb = Input.GetAxis("Vertical");
+        Vector2 climbVelocity = new Vector2(myRigidBody.velocity.x, ladderClimb * climbSpeed);
+        myRigidBody.velocity = climbVelocity;
+
+        bool playerHasVerticalSpeed = Mathf.Abs(myRigidBody.velocity.y) > Mathf.Epsilon;
+        myAnimator.SetBool("Climbing", playerHasVerticalSpeed);
     }
 }
