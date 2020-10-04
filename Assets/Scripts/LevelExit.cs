a9c2cbf4385c377d93e4741b3d49f7c64f4fab32
TheWrigglyGhost <serenokaisser@gmail.com>
2020-10-04 22:47:08 +0800

    Added Level Exit feature... made a particle effect for it by myself

---
+++ Assets/Scripts/LevelExit.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        OnLevelExit();
+    }
+
+    private void OnLevelExit()
+    {
+        StartCoroutine(LoadNextScene());
+    }
+
+    IEnumerator LoadNextScene()
+    {
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        
+        yield return new WaitForSecondsRealtime(2f);
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
