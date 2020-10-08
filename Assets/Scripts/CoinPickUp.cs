8a0f13ed8d2aee0547e58c724db747c3f86727fc
TheWrigglyGhost <serenokaisser@gmail.com>
2020-10-08 19:00:49 +0800

    Added coin pick up vfx and sfx

--- Assets/Scripts/CoinPickUp.cs
+++ Assets/Scripts/CoinPickUp.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class CoinPickUp : MonoBehaviour
 {
+
+    [SerializeField] AudioClip coinPickUpSFX;
+    [SerializeField] GameObject coinPickUpVFX;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        GameObject coinPickUpVFXObject = Instantiate(coinPickUpVFX, transform.position, Quaternion.identity);
+        AudioSource.PlayClipAtPoint(coinPickUpSFX, Camera.main.transform.position);
         Destroy(gameObject);
+        Destroy(coinPickUpVFXObject, 0.3f);
     }
 }
